07a71158692877b945f9eedf8fa6ea9aec5fd4fc
Anibal Velarde <anibalvelarde@gmail.com>
2026-02-24 20:14:58 -0600

    Added format-check priour to URL interpolation.

--- app-ui/src/services/SessionsHttpClient.ts
+++ app-ui/src/services/SessionsHttpClient.ts
@@ -5,6 +5,9 @@ import { Appointment } from '../interfaces/Appointment';
 export class SessionsHttpClient extends HttpClientBase {
   async getSessions(date: string): Promise<Appointment[]> {
     const formattedDate = this.formatDateForApi(date);
+    if (!/^\d{4}-\d{2}-\d{2}$/.test(formattedDate)) {
+      throw new Error("Invalid date format.");
+    }
     const url = `/api/Sessions/${formattedDate}/all`;
     return this.get<Appointment[]>(url);
   }
